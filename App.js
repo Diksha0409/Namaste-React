00d14d608344cb1a5946d959965d366ccecfae02
diksha2210 <dikshaparashar22@gmail.com>
2024-02-13 17:26:22 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React.createElememt =>  is an ReactElement(JS Object) no an HTML Element => when we do 'render' it becomes HTML Element
+//****************************Creating ReactElement via Core React********************************************
 const heading = React.createElement(
-    "h1",
-    { id: "heading", abc:"xyz"},
-    "Hello World from React"
+  "h1",
+  { id: "heading" },
+  "Hello Chaitananya ;P"
 );
+console.log(heading);
+
+//JSX is not HTML in Javascript it is HTML-like or XML-like syntax.
+//JSX (transpiled before it reaches to JS engine) - Parcel- Babel
+//JSX =>Babel transpiles to React.createElement => ReactElement- JS Object => HTMLElement(on Render)
+//**************************Creating ReactElement via JSX*******************************
+
+const elem = <span>React Element</span>;
+//const jsxHeading = <h1 className="head">Hello Chaitanya using JSX</h1>; //React Element
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Hello Chaitanya using JSX
+  </h1>
+); //React Element
+
+//React Functional Component
+const HeadingComponent = () => {
+  return <h1 className="heading">Namaste React Functional Component</h1>;
+};
+
+// can also write as shown below as we are using arrow function, we can remove 'return' as using arrow function
+//for calling one component into another component just write component name within <> like <Title />
+//below is **Component Composition** means one component into another
+const HeadingComponent2 = () => (
+  <div id="container">
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
-console.log(heading);
+//root.render(jsxHeading);
+root.render(<HeadingComponent2 />); //for renderng a Component we use variable in angular <> bracket as shown
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 Namaste React Practice!!
+
+
+What Parcel is doinf for us:
+-created Dev build
+-created local server
+-automaticaly refreshes our page which is called HMR(Hot Module Replacement)
+-it uses a File Watching Algorithm (written in C++)which see all changes we do and reflect it to server. it keeps track of all the save we do in project and build our app again n again.
+-it uses caching which gives faster builds (.parcel-cache).
+-does Image optmization
+-if we do production build it will minify file also i.e. does minification
+-will do bundling
+-will compress our file
+-consistent hashing-- *****search more on google***********
+-code spliting (split your files)
+-differential bundling -support older browsers (it helps app to open in latest and older different browsers)
+-diganostics
+-good error handling view
+-allows to run on HTTPS (SSL) if needed 
+-tree shaking algorithm- remove unused code fr us
+-different dev and production bundle
+
+********************READ Parceljs.org documentation******************************
